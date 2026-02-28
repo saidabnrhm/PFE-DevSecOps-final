e37113ab916d2c14570db60a3f4f752da3581a85
Sinda Ben Rhouma <sindabnrhm@gmail.com>
2026-02-28 13:42:07 +0100

    Fix CI remove SpringBootTest

--- pfeFinalBack-master/src/test/java/tn/arabsoft/auth/AuthpfeApplicationTests.java
+++ pfeFinalBack-master/src/test/java/tn/arabsoft/auth/AuthpfeApplicationTests.java
@@ -1,11 +1,7 @@
 package tn.arabsoft.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-
-@SpringBootTest
 class AuthpfeApplicationTests {
 
 	@Test
